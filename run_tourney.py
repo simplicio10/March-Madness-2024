ce81620345e0e6b9206cb23975384fb3d854e7d6
simplicio10 <claypas@gmail.com>
2024-03-18 16:43:47 -0500

    Build prototype bracket

--- run_tourney.py
+++ run_tourney.py
@@ -1,6 +1,29 @@
-from mens import mens_prototype as tm
+from mens import mens_prototype as mp
 
-houston = tm.Team('Houston', 'South', 1)
-marquette = tm.Team('Marquette')
+houston = mp.Team('Houston', 'South', 1)
+marquette = mp.Team('Marquette', 'South', 2)
+kentucky = mp.Team('Kentucky', 'South', 3)
+duke = mp.Team('Duke', 'South', 4)
+wisconsin = mp.Team('Wisconsin', 'South', 5)
+tex_tech = mp.Team('Texas Tech', 'South', 6)
+florida = mp.Team('Florida', 'South', 7)
+nebraska = mp.Team('Nebraska', 'South', 8)
 
-print(houston.get_seed()) 
+test_bracket = [
+    houston,
+    marquette,
+    kentucky,
+    duke,
+    wisconsin,
+    tex_tech,
+    florida,
+    nebraska
+]
+
+print(mp.setMatchups(test_bracket))
+
+def set_matchups(teams):
+    pass
+
+def play_tourney(matchups):
+    pass
