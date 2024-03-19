544c958cc1e605543237506d10f9db524d0992e0
simplicio10 <claypas@gmail.com>
2024-03-19 08:50:40 -0500

    Add play region function

--- libraries/mens_prototype.py
+++ libraries/mens_prototype.py
@@ -45,10 +45,10 @@ def set_matchups(teams):
         return games
 
 #Write test for function
-def play_region(matchups):
+def play_round(matchups):
     
     winners = []
-
+        
     for matchup in matchups.values():
 
         if matchup[0].getSeed() < matchup[1].getSeed():     
@@ -74,5 +74,36 @@ def play_region(matchups):
                         [matchup[0], matchup[1]], 
                         p=[wp.win_probs[0], wp.win_probs[0]])
             winners.append(winner)
-            
+
     return winners
+
+def play_region(teams):
+    rem_teams = teams
+
+    while len(rem_teams) > 2:
+        rem_teams = (play_round(set_matchups(rem_teams)))
+        print(rem_teams)
+    
+    if rem_teams[0].getSeed() < rem_teams[1].getSeed():     
+        high_seed = rem_teams[0].getSeed()
+        low_seed = rem_teams[1].getSeed()
+        diff = low_seed - high_seed
+        winner = np.random.choice(
+                    [rem_teams[0], rem_teams[1]], 
+                    p=[wp.win_probs[diff], (1-wp.win_probs[diff])])
+        return winner
+
+    elif rem_teams[0].getSeed() > rem_teams[1].getSeed():
+        high_seed = rem_teams[1].getSeed()
+        low_seed = rem_teams[0].getSeed()
+        diff = low_seed - high_seed
+        winner = np.random.choice(
+                    [rem_teams[0], rem_teams[1]], 
+                    p=[(1-wp.win_probs[diff]), wp.win_probs[diff]])
+        return winner
+
+    else:
+        winner = np.random.choice(
+                    [rem_teams[0], rem_teams[1]], 
+                    p=[wp.win_probs[0], wp.win_probs[0]])
+        return winner
--- run_tourney.py
+++ run_tourney.py
@@ -1,5 +1,4 @@
 from libraries import mens_prototype as mp
-#from libraries import win_probabilities as wp
 import numpy as np
 
 houston = mp.Team('Houston', 'South', 1)
@@ -10,13 +9,29 @@ wisconsin = mp.Team('Wisconsin', 'South', 5)
 tex_tech = mp.Team('Texas Tech', 'South', 6)
 florida = mp.Team('Florida', 'South', 7)
 nebraska = mp.Team('Nebraska', 'South', 8)
+tex_am = mp.Team('Texas A&M', 'South', 9)
+colorado = mp.Team('Colorado', 'South', 10)
+nc_state = mp.Team('North Carolina State', 'South', 11)
+mcneese_st = mp.Team('McNeese State', 'South', 12)
+samford = mp.Team('Samford', 'South', 13)
+akron = mp.Team('Akron', 'South', 14)
+western_ky = mp.Team('Western Kentucky', 'South', 15)
+longwood = mp.Team('Longwood', 'South', 16)
 
 test_bracket = [
+    western_ky,
     houston,
+    tex_am,
+    colorado,
+    samford,
     marquette,
     kentucky,
+    nc_state,
     duke,
+    mcneese_st,
+    longwood,
     wisconsin,
+    akron,
     tex_tech,
     florida,
     nebraska
@@ -24,4 +39,4 @@ test_bracket = [
 
 #print(mp.set_matchups(test_bracket))
 
-print(mp.play_region(mp.set_matchups(test_bracket)))
+print(mp.play_region(test_bracket))
